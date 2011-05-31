9dc3133f967559fcc1d9fb7a7235e9f042d13d70
Clément Uster <clement.uster@gmail.com>
2011-05-31 23:07:37 +0200

    add the item

---
+++ src/main/java/org/tapestry5/evangelist/entities/Item.java
@@ -0,0 +1,52 @@
+package org.tapestry5.evangelist.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.apache.tapestry5.beaneditor.NonVisual;
+
+@Entity
+public class Item {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonVisual
+	private long id;
+	
+	private String name;
+	private String description;
+	private long price;
+	
+	
+	public Item() {
+		super();
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	
+	
+}
