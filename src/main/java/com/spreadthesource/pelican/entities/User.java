d3d3e4bcf2be470d69c9f73827631e2c475bec25
Gillespie59 <demey.emmanuel@gmail.com>
2011-06-01 00:03:26 +0200

    Forgot some files

---
+++ src/main/java/com/spreadthesource/pelican/entities/User.java
@@ -0,0 +1,47 @@
+package com.spreadthesource.pelican.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.apache.tapestry5.beaneditor.NonVisual;
+
+@Entity
+public class User {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonVisual
+	private long id;
+	
+	private String name;
+	private String email;
+	private String password;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+		
+}
---
+++ src/main/java/com/spreadthesource/pelican/pages/Index.java
@@ -0,0 +1,52 @@
+package com.spreadthesource.pelican.pages;
+
+import java.util.List;
+
+import org.apache.tapestry5.EventConstants;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import com.spreadthesource.pelican.entities.Item;
+
+
+public class Index
+{
+	@Property
+	private Item currentItem;
+	
+	@Property
+	private List<Item> listOfItems;
+	
+	@Inject
+	private Session session;
+	
+	
+	@SetupRender
+	public void setupRender(){
+		listOfItems = session.createCriteria(Item.class).addOrder(Order.asc("id")).list();
+	}
+	
+	@CommitAfter
+	@OnEvent(value=EventConstants.ACTION,component="increase")
+	public void increase(long id){
+		
+		Item item = (Item)session.createCriteria(Item.class).add(Restrictions.eq("id", id));
+		item.setPrice(item.getPrice()+1);
+		session.saveOrUpdate(item);
+		
+	}
+	
+	@CommitAfter
+	@OnEvent(value=EventConstants.ACTION,component="decrease")
+	public void decrease(long id){
+		Item item = (Item)session.createCriteria(Item.class).add(Restrictions.eq("id", id));
+		item.setPrice(item.getPrice()-1);
+		session.saveOrUpdate(item);
+	}
+}
