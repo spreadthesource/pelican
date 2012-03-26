6e4ca05005900c485b88c4952e6a530b5fc41b75
Robin Komiwes <odisseus@gmail.com>
2012-03-26 13:54:57 +0200

    Code Style refactor, mainly

--- src/main/java/com/spreadthesource/pelican/entities/Item.java
+++ src/main/java/com/spreadthesource/pelican/entities/Item.java
@@ -11,85 +11,118 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
-public class Item {
+public class Item
+{
 
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
-	private String name;
-	
-	private String description;
-	
-	private long price;
-	
-	private String image;
-	
-	@ManyToOne
-	private User user;
-	
-	private Date expireDate;
-	
-	@OneToMany(mappedBy="item")
-	private Collection<Bid> bids;
-	
-	
-	
-	public String getImage() {
-		return image;
-	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public Date getExpireDate() {
-		return expireDate;
-	}
-	public void setExpireDate(Date expireDate) {
-		this.expireDate = expireDate;
-	}
-	public Collection<Bid> getBids() {
-		return bids;
-	}
-	public void setBids(Collection<Bid> bids) {
-		this.bids = bids;
-	}
-	public Item() {
-		super();
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public long getPrice() {
-		return price;
-	}
-	public void setPrice(long price) {
-		this.price = price;
-	}
-	
-	@Override
-	public String toString() {
+    private long id;
 
-		return this.name;
-	}
+    private String name;
+
+    private String description;
+
+    private long price;
+
+    private String image;
+
+    @ManyToOne
+    private User user;
+
+    private Date expireDate;
+
+    @OneToMany(mappedBy = "item")
+    private Collection<Bid> bids;
+
+    public String getImage()
+    {
+        return image;
+    }
+
+    public void setImage(String image)
+    {
+        this.image = image;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    public Date getExpireDate()
+    {
+        return expireDate;
+    }
+
+    public void setExpireDate(Date expireDate)
+    {
+        this.expireDate = expireDate;
+    }
+
+    public Collection<Bid> getBids()
+    {
+        return bids;
+    }
+
+    public void setBids(Collection<Bid> bids)
+    {
+        this.bids = bids;
+    }
+
+    public Item()
+    {
+        super();
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public long getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(long price)
+    {
+        this.price = price;
+    }
+
+    @Override
+    public String toString()
+    {
+
+        return this.name;
+    }
 }
--- src/main/java/com/spreadthesource/pelican/entities/User.java
+++ src/main/java/com/spreadthesource/pelican/entities/User.java
@@ -21,8 +21,6 @@ public class User {
 	
 	private String name;
 	
-	
-	
 	private String email;
 	
 	private String password;
